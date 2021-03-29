015cf9afbb5132013cedf4d144e623cbd483650a
jay <jaysuthar0603@gmail.com>
2021-03-29 06:05:45 +0530

    update 44th

--- projecteuler/44.py
+++ projecteuler/44.py
@@ -5,14 +5,15 @@ a=[]
 for i in range(1,5000):
     p=(i*(3*i-1))//2
     a.append(p)
-
+b=[]
 for j in range(len(a)):
     for k in range(len(a)):
         if (a[j]+a[k] in a):
             if a[j]>a[k]:
                 if (a[j] - a[k] in a):
-                    print(a[j], " ", a[k]," ",a[j]-a[k])
+                    b.append(a[j]-a[k])
             else:
                 if (a[k] - a[j] in a):
-                    print(a[j], " ", a[k]," ",a[k]-a[j])
+                    b.append(a[k]-a[j])
 
+print(min(b))
