e701563249d55ec0f7cf71f14c12088350dbfd97
jay <jaysuthar0603@gmail.com>
2022-03-31 17:37:17 +0530

    codechef Increase-IQ solution added

---
+++ codechef/Increase IQ.cpp
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int x;
+	cin>>x;
+	x=x+7;
+	if(x>170){
+		cout<<"YES"<<endl;
+	}else{
+		cout<<"NO"<<endl;
+	}
+	
+			
+ 	return 0;
+ 	
+}
