f0612d353006abbb5fa036d89f87ca8ef27fa593
jay <jaysuthar0603@gmail.com>
2022-04-06 10:39:21 +0530

    codechef count the notebooks solution added

---
+++ codechef/Count the Notebooks.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int t;
+	cin>>t;
+	while(t--){
+		int x; cin>>x;
+		x = ((x*1000)/100);
+		cout<<x<<endl;
+	}
+return 0;
+}
