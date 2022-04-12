0ec9383650ea98e63529c50504e3df081121215c
jay <jaysuthar0603@gmail.com>
2022-04-13 02:40:54 +0530

    codechef minimum cars required solution added

---
+++ codechef/Minimum Cars required.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	int t; cin>>t;
+	while(t--){
+		int n; cin>>n;
+		if(n%4 == 0) cout<<n/4<<endl;
+		else cout<<(n/4)+1<<endl;
+	
+		
+	}
+return 0;
+}
