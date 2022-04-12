2c7bfe26caab2d7a028111e5f15a4163131d90b1
jay <jaysuthar0603@gmail.com>
2022-04-13 02:37:09 +0530

    codechef monthly budget solution added

---
+++ codechef/Monthly Budget.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	int t; cin>>t;
+	while(t--){
+		int x,y; cin>>x>>y;
+		if((y*30)<=x) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	
+		
+	}
+return 0;
+}
