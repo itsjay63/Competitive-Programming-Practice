284973a7f592690e5f34c5b4913e28f26a4ce8fd
jay <jaysuthar0603@gmail.com>
2022-04-16 01:27:27 +0530

    codechef black cells in a chessboard solution added

---
+++ codechef/Black cells in a chessboard.cpp
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	
+	int n; cin>>n;
+	cout<<(n/2) * n<<endl;
+		
+	
+return 0;
+}
