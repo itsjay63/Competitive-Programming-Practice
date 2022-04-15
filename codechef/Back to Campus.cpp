01424f6a01702853c66f848f43b4d38840c0e957
jay <jaysuthar0603@gmail.com>
2022-04-16 01:53:19 +0530

    codechef back to campus solution added

---
+++ codechef/Back to Campus.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k; cin>>n>>k;
+		cout<<ceil((double)n/k)<<endl;	
+		
+	}
+	return 0;
+}
