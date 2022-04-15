accf05f36e27b49a0fe3586643320e5f4d6e99d6
jay <jaysuthar0603@gmail.com>
2022-04-16 01:43:07 +0530

    codechef chess match solution added

---
+++ codechef/Chess Match.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int t;
+	cin>>t;
+	while(t--){
+		int n,a,b; cin>>n>>a>>b;
+		cout<<(2*(180+n)-a-b)<<endl;	
+		
+	}
+	return 0;
+}
