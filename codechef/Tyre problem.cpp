4b211a24ff5d4f66608eb1af768ecb518f62af4f
jay <jaysuthar0603@gmail.com>
2022-04-19 15:20:07 +0530

    codechef tyre problem solution added

---
+++ codechef/Tyre problem.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int t; cin>>t;
+	while(t--){
+		int x,y; cin>>x>>y;
+		cout<<x*2 + y*4<<endl;
+	}
+	return 0;
+}
