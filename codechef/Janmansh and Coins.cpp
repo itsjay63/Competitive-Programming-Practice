1154dc00b86c90b6debe98647d5c3dbec341a787
jay <jaysuthar0603@gmail.com>
2022-04-19 15:58:03 +0530

    codechef janmansh and coins solution added

---
+++ codechef/Janmansh and Coins.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int t; cin>>t;
+	while(t--){
+		int x,y; cin>>x>>y;
+		cout<<x*10 + y*5<<endl;
+	}	
+	return 0;
+}
