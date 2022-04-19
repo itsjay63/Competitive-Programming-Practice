f181a087c344ff4664e2ec088a025bef925681a0
jay <jaysuthar0603@gmail.com>
2022-04-19 15:07:15 +0530

    codechef email remainders solution added

---
+++ codechef/Email Reminders.cpp
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int n,u; cin>>n>>u;
+	cout<<n-u<<endl;
+	
+	return 0;
+}
