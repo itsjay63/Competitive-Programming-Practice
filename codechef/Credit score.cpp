1d4ccbac88c44b601d6fd562d99b5b3fd375dd1f
jay <jaysuthar0603@gmail.com>
2022-04-19 15:43:02 +0530

    codechef credit score solution added

---
+++ codechef/Credit score.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int x; cin>>x;
+	if(x>=0 && x<=1000){
+		if(x>=750) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	}	
+	
+	return 0;
+}
