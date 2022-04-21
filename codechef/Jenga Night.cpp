3c4a54cf0889baabe3e3d305dac8b2f8d6c4e64e
jay <jaysuthar0603@gmail.com>
2022-04-21 12:11:25 +0530

    codechef jenga night solution added

---
+++ codechef/Jenga Night.cpp
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------------------
+//-------------------Author : itsjaysuthar ----------------------------------------------
+//---------------------------------------------------------------------------------------
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{ 
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif 
+	
+	int t; cin>>t;
+	while(t--){
+		int n,x; cin>>n>>x;
+		if(x%n==0) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+		
+	}
+	
+		
+}
