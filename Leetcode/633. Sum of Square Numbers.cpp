25bb8ce31f4642d43217b4f6d6a725422e15ceff
jay <jaysuthar0603@gmail.com>
2022-12-10 15:14:11 -0600

    633 sum of square numbers leetcode solution added

---
+++ Leetcode/633. Sum of Square Numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        if(c==0){
+            return true;
+        }
+        for(long a=0; a*a<c;a++){
+            double b = sqrt(c-a*a);
+            if(b == int(b)){
+                return true;
+            }   
+        }
+        return false;
+    }   
+};
