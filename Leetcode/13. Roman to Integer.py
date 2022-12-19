e302f1f82e450bfc69cbeece3452150a8ca3bf8b
jay <jaysuthar0603@gmail.com>
2022-12-19 04:29:21 -0600

    13 roman to integer leetcode solution added

---
+++ Leetcode/13. Roman to Integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        nums ={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        for i in s:
+            sum = sum + nums[i]
+        return sum
+
+        
