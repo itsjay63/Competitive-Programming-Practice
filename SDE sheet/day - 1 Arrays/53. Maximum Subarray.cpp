9d69e66b66a7ad4468afc7c1f6783df8bdf0b288
itsjaysuthar <jaysuthar0603@gmail.com>
2023-06-14 07:24:30 -0500

    53 maximum subarray added

---
+++ SDE sheet/day - 1 Arrays/53. Maximum Subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = INT_MIN;
+        int sum = 0;
+        
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+
+            if(sum > maxi){
+                maxi = sum;
+            }
+            
+            if(sum<0){
+                sum = 0;
+            }
+
+            
+        }
+        return maxi;
+
+    }
+};
